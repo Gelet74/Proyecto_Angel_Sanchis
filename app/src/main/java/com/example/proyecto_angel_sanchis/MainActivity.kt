4cc88008763f6f194fb0c5dde9c9671e3b5c57fd
Gelet74 <sanchis74@gmail.com>
2026-02-09 13:47:15 +0100

    09/02 13:50

--- app/src/main/java/com/example/proyecto_angel_sanchis/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_angel_sanchis/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Proyecto_Angel_SanchisTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               App()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Proyecto_Angel_SanchisTheme {
-        Greeting("Android")
-    }
-}
